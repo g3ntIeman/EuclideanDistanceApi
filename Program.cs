61e3ff04a3c63c2571e9cf5df3c700f1209c927f
Никита Гунько <nikita.gunko123@mail.ru>
2026-01-20 12:23:23 +0300

    Реализация минимального API проекта

---
+++ Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Swagger UI
+app.UseSwagger();
+app.UseSwaggerUI();
+
+// DTO
+app.MapPost("/distance", (DistanceRequest req) =>
+{
+    var dx = req.X2 - req.X1;
+    var dy = req.Y2 - req.Y1;
+
+    var distance = Math.Sqrt(dx * dx + dy * dy);
+
+    return Results.Ok(new DistanceResponse(distance));
+})
+.WithName("GetEuclideanDistance")
+.WithOpenApi();
+
+app.Run();
+
+public record DistanceRequest(double X1, double Y1, double X2, double Y2);
+public record DistanceResponse(double Distance);
